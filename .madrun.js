042b56b3a6796e07af95c714f3b0e70b56b11e8f
Jason Caldwell <jaswrks@o5p.me>
2023-01-25 18:20:18 -0500

    Project update. [p][robotic]

--- .madrun.js
+++ .madrun.js
@@ -5,28 +5,34 @@
 
 export default {
 	'update:project': async (args) => {
-		const cmds = ['npm update --save'];
-
-		if (args.repos) {
-			const repoUpdateCMD =
-				'if git rev-parse --is-inside-work-tree &>/dev/null; then' +
-				(args.pkgs
-					? '	if [[ -n "$(git status --short 2>/dev/null)" ]]; then' +
-					  "		git add --all && git commit --message 'Project update. [p][robotic]';" +
-					  '	fi;' +
-					  '	if [[ "$(git symbolic-ref --short --quiet HEAD)" == main ]]; then' +
-					  '		npm version patch;' +
-					  '		npm publish;' +
-					  '	fi;'
-					: '') +
-				'	if [ -n "$(git status --short 2>/dev/null)" ]; then' +
-				"		git add --all && git commit --message 'Project update. [p][robotic]';" +
-				'	fi;' +
-				'	git push --set-upstream origin "$(git symbolic-ref --short --quiet HEAD)";' +
-				'	git push origin --tags;' +
-				'fi';
-			cmds.push(repoUpdateCMD);
-		}
-		return cmds;
+		return {
+			env: {
+				GH_TOKEN: process.env.USER_GITHUB_TOKEN || '',
+				GITHUB_TOKEN: process.env.USER_GITHUB_TOKEN || '',
+				NPM_TOKEN: process.env.USER_NPM_TOKEN || '',
+				CLOUDFLARE_API_TOKEN: process.env.USER_CLOUDFLARE_TOKEN || '',
+			},
+			cmds: [
+				'npm update --save;',
+				...(args.repos ? [
+					' if git rev-parse --is-inside-work-tree &>/dev/null; then' +
+					(args.pkgs
+						? '	if [[ -n "$(git status --short 2>/dev/null)" ]]; then' +
+						"		git add --all && git commit --message 'Project update. [p][robotic]';" +
+						'	fi;' +
+						'	if [[ "$(git symbolic-ref --short --quiet HEAD)" == main ]]; then' +
+						'		npm version patch;' +
+						'		npm publish;' +
+						'	fi;'
+						: '') +
+					'	if [ -n "$(git status --short 2>/dev/null)" ]; then' +
+					"		git add --all && git commit --message 'Project update. [p][robotic]';" +
+					'	fi;' +
+					'	git push --set-upstream origin "$(git symbolic-ref --short --quiet HEAD)";' +
+					'	git push origin --tags;' +
+					' fi;'
+				] : []),
+			],
+		};
 	},
 };
